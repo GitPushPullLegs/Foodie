fcca49bb6fc93c043b2f75c89cd104dbae5d3e24
Jose Aguilar <jose.aguilar.6694@gmail.com>
2021-02-17 17:53:10 -0800

    Initial Commit

---
+++ Foodie/FoodieApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodieApp.swift
+//  Foodie
+//
+//  Created by Jose Aguilar on 2/17/21.
+//
+
+import SwiftUI
+
+@main
+struct FoodieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
